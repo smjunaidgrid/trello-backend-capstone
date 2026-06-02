a885d689fefc7ef407493457bb0a38a2a925bd06
smjunaidgrid <moshaik@griddynamics.com>
2026-06-02 15:06:17 +0530

    api,db,schemas,models

--- app/api/v1/auth.py
+++ app/api/v1/auth.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
+from app.api.dependencies import get_current_user
+from app.models.user import User
 from app.schemas.user import (
     UserCreate,
     UserResponse,
@@ -43,3 +45,12 @@ async def login_user(
         user_credentials.email,
         user_credentials.password
     )
+@router.get(
+    "/me",
+    response_model=UserResponse
+)
+async def get_me(
+    current_user: User = Depends(get_current_user)
+):
+
+    return current_user 
--- app/db/base.py
+++ app/db/base.py
@@ -1,3 +1,5 @@
 from app.db.database import Base
 
 from app.models.user import User
+
+from app.models.board import Board
